647f2999826194273922d3dab3631e4d30df6657
LizzieHerman <lizziemn96@gmail.com>
2016-10-26 12:48:13 -0600

    initial

---
+++ src/machinelearningexperimentation/MachineLearningExperimentation.java
@@ -0,0 +1,14 @@
+package machinelearningexperimentation;
+
+/**
+ *
+ * @author Lizzie Herman
+ * @author Ryan Freivalds
+ * @author Connor O’Leary
+ * @author Greg Neznanski
+ */
+public class MachineLearningExperimentation {
+    public static void main(String[] args) {
+        
+    }
+}
