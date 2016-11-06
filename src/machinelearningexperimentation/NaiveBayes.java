5d267a94f66d061a150f0d06855d91b8afdfd321
freivalds1 <freivalds1@gmail.com>
2016-11-06 14:37:31 -0700

    implementing template pattern
    see classAlg for details

--- src/machinelearningexperimentation/NaiveBayes.java
+++ src/machinelearningexperimentation/NaiveBayes.java
@@ -3,7 +3,13 @@ package machinelearningexperimentation;
 /**
  *
  * @author Lizzie Herman
+ * Ryan Freivalds
  */
-public class NaiveBayes {
+public class NaiveBayes extends classAlg{
+    //needs a constructor to accept a dataset that's been filled if needed, and it's own classification implementation
 
+ @Override
+public double algorithm(dataObject[] train, dataObject[] test){
+     return 0;
+}
 }
