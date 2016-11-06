f20add741aac976c5fad462562695c75d34e68e1
freivalds1 <freivalds1@gmail.com>
2016-11-06 15:08:20 -0700

    details
    our current understanding of how to handle data.

--- src/machinelearningexperimentation/dataObject.java
+++ src/machinelearningexperimentation/dataObject.java
@@ -10,7 +10,12 @@ package machinelearning;
  * @author s53q433
  */
 public class dataObject {
+    int size; //how many attributes an entry in a dataset has
+    String[] dataEntry = new String[size]; //an array meant to hold each attribute of a dataset in it's own index
+    String[] dataType = new String[size]; //lables each attribute continuous or discrete, if it's a classficiation, etc. How that data entry needs to be handled.
     
+    //as of now, we think to read in each dataset, that can have 600+ entries, with up to 35 different kinds of attributes that need to be handled in different ways, 
+    //we're going to need a soybean method, voting method, etc that reads in all the entries (and fills them, if need be) in their own distinct way.
     public dataObject(){
         
     }
