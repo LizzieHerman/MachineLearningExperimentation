a5733dbf2e48ab26fb421cfb8db6672a2ac2443e
freivalds1 <freivalds1@gmail.com>
2016-11-06 14:34:31 -0700

    now with 1000% less error
    capitolized "TAN."

--- src/machinelearningexperimentation/TAN.java
+++ src/machinelearningexperimentation/TAN.java
@@ -5,7 +5,7 @@ package machinelearningexperimentation;
  * @author Lizzie Herman
  Ryan Freivalds s53q433
  */
-public class tan extends classAlg{
+public class TAN extends classAlg{
     //needs a constructor to accept a dataset that's been filled if needed, and it's own classification implementation
 
  @Override
