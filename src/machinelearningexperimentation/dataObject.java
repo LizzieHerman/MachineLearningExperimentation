db0088b135ac92f23817c04bc8d0ab0218abb5da
freivalds1 <freivalds1@gmail.com>
2016-11-06 14:46:45 -0700

    Initial.
    Not sure how we're going to handle data sets, but right now I see them as an array of "dataObjects," a data object being one entry, the object holding an array or whatever int/discrete/categorical values/attributes. a single entry possesses.

---
+++ src/machinelearningexperimentation/dataObject.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package machinelearning;
+
+/**
+ *
+ * @author s53q433
+ */
+public class dataObject {
+    
+    public dataObject(){
+        
+    }
+}
