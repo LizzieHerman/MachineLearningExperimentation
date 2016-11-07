235501f8a54d7e1824e54123d032f0912fa9556d
LizzieHerman <lizziemn96@gmail.com>
2016-11-07 14:05:30 -0700

    Revert "read files into arrays"
    This reverts commit 2cb45e1593f6f022c3e9bcb6f662ef27fa25465a.

--- src/machinelearningexperimentation/MachineLearningExperimentation.java
+++ src/machinelearningexperimentation/MachineLearningExperimentation.java
@@ -7,87 +7,8 @@ package machinelearningexperimentation;
  * @author Connor O’Leary
  * @author Greg Neznanski
  */
-
-import java.io.*;
 public class MachineLearningExperimentation {
-    static String[][] cancer = new String[699][11];
-    static String[][] glass = new String[214][11];
-    static String[][] iris = new String[150][5];
-    static String[][] soybean = new String[47][36];
-    static String[][] vote = new String[435][17];
-
     public static void main(String[] args) {
-       readFiles();
-       
-       
-    }
-    
-    static void readFiles(){
-        String cancerFile = "breast-cancer-wisconsin.data.txt";
-        String glassFile = "glass.data.txt";
-        String irisFile = "iris.data.txt";
-        String soybeanFile = "soybean-small.data.txt";
-        String voteFile = "house-votes-84.data.txt";
-        // these files need to be in theproject folder for this to work
         
-        BufferedReader br = null;
-        String line = "";
-        String cvsSplitBy = ",";
-        
-        try {
-            br = new BufferedReader(new FileReader(cancerFile));
-            int i = 0;
-            line = br.readLine();
-            while (line != null) {
-                cancer[i] = line.split(cvsSplitBy);
-                i++;
-                line = br.readLine();
-            }
-            br.close();
-        
-            br = new BufferedReader(new FileReader(glassFile));
-            i = 0;
-            line = br.readLine();
-            while (line != null) {
-                glass[i] = line.split(cvsSplitBy);
-                i++;
-                line = br.readLine();
-            }
-            br.close();
-        
-            br = new BufferedReader(new FileReader(irisFile));
-            i = 0;
-            line = br.readLine();
-            while (line != null) {
-                iris[i] = line.split(cvsSplitBy);
-                i++;
-                line = br.readLine();
-            }
-            br.close();
-            
-            br = new BufferedReader(new FileReader(soybeanFile));
-            i = 0;
-            line = br.readLine();
-            while (line != null) {
-                soybean[i] = line.split(cvsSplitBy);
-                i++;
-                line = br.readLine();
-            }
-            br.close();
-       
-            br = new BufferedReader(new FileReader(voteFile));
-            i = 0;
-            line = br.readLine();
-            while (line != null) {
-                vote[i] = line.split(cvsSplitBy);
-                i++;
-                line = br.readLine();
-            }
-            br.close();
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
     }
 }
