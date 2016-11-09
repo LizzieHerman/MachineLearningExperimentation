689d30ba053e49d8554a7d3861e49938905e104a
freivalds1 <freivalds1@gmail.com>
2016-11-09 13:57:49 -0700

    changed from "dataobjects[]" to "Strings[][]"
    to better integrate with how we read in the data

--- src/machinelearningexperimentation/classAlg.java
+++ src/machinelearningexperimentation/classAlg.java
@@ -16,10 +16,11 @@ package machinelearningexperimentation;
 //the actual algorithm implementation from it's subclass objects.
 public abstract class classAlg { 
     public int size; //how many entries are being read in
-    dataObject[] sortSet = new dataObject[size]; //represents our raw input
-    dataObject[] readySet = new dataObject[size]; //uses the filled/read in input to produce an array representing our training and testing set.
-    dataObject[] trainingSet;
-    dataObject[] testSet; 
+	public int attSize; //how many attributes belong to a single entry
+    String[][] sortSet; //represents our raw input
+    String[][] readySet; //uses the filled/read in input to produce an array representing our training and testing set.
+    String[][] trainingSet;
+    String[][] testSet; 
     
 //might be a pain to read in all those values-- since we'll need to differentiate what we want the variables to be
 //or we take the time to hard code them in, unless someone is expereience in how to read in largs amounts of data.
@@ -45,19 +46,19 @@ public abstract class classAlg {
     
     
     
-    public dataObject[] readSet(dataObject[] newInput){
+    public String[][] readSet(String[][] newInput){
         //needs to find or read in one data set for the algorithm to run on, and put it in an array.
         //returns that array for use.
         return null;
     }
     
-    public void fillSet(dataObject[] dataEntries){
+    public void fillSet(String[][] dataEntries){
         //returns nothing, as it fills in any empty values for use in all future runs.
         //this may need to occure "outside" of this process, because EVERY algorithm needs to use the same filled in values.
         //enless we can ensure this method fills in the entries with the same values each time (unlikely).
     }
 
-    private dataObject[] newSets(dataObject[] sortSet) {
+    private String[][] newSets(String[][] sortSet) {
         //This method represents our 5 x 2 "folding" for cross validation.
         //All this method does is take our filled in input array, and shuffles the indexes into a new array, using every index
         //the first half of this new array represents out training set, the last half our testing set.
@@ -72,7 +73,7 @@ public abstract class classAlg {
     }
     
 
-    private double classifyTest(dataObject[] test) {
+    private double classifyTest(String[][] test) {
         /*this method accepts the ready datasets, calls seperate to cut them into training/testing halves,
         then runs the classification algorithm as persribed. It then calls newSets and repeats however many times we need it to
         before returning the average of each newSet call and classification run*/
@@ -89,7 +90,7 @@ public abstract class classAlg {
         return 0;
     }
     
-    public abstract double algorithm(dataObject[] train, dataObject[] test);
+    public abstract double algorithm(String[][] train, String[][] test);
         //implemented by each individual class/algorithm. Not here. returns the accuracy for a single training/test set run
     
     
