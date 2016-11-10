5186e93dce0e1a0bfcee87b86c3f44e0d945a0f9
LizzieHerman <lizziemn96@gmail.com>
2016-11-10 14:19:04 -0700

    discretized the continuous values

--- src/machinelearningexperimentation/MachineLearningExperimentation.java
+++ src/machinelearningexperimentation/MachineLearningExperimentation.java
@@ -25,7 +25,7 @@ public class MachineLearningExperimentation {
        // TO-DO get rid of the unknown values
        replaceUnknowns();
        // TO-DO convert continuous values to discrete ones
-       
+       discretize(10);
        // split data into training and data sets
        
        // Run these for each algorithm
@@ -139,6 +139,62 @@ public class MachineLearningExperimentation {
                      * so it fits with the other data better
                      */
                 }
+            }
+        }
+    }
+    
+    static void discretize(int n){
+        double[][] irisExtrema = {{4.3,2,1,0.1,-1},{7.9,4.4,6.9,2.5,-1}};
+        double[][] glassExtrema = {{-1,1.5112,10.73,0,0.29,69.81,0,5.43,0,0,-1},
+            {-1,1.5339,17.38,4.49,3.5,75.41,6.21,16.19,3.15,0.51,-1}};
+        
+        double increment = 0;
+        double[][] irisVals = new double[5][n];
+        for(int i = 0; i < irisVals.length; i++){
+            irisVals[i][0] = irisExtrema[0][i];
+            increment = (irisExtrema[1][i] - irisExtrema[0][i]) / n;
+            for(int j = 1; j < n-1; j++){
+                irisVals[i][j] = irisVals[i][j-1] + increment;
+            }
+            irisVals[i][n-1] = irisExtrema[1][i];
+        }
+        double[][] glassVals = new double[11][n];
+        for(int i = 0; i < glassVals.length; i++){
+            glassVals[i][0] = glassExtrema[0][i];
+            increment = (glassExtrema[1][i] - glassExtrema[0][i]) / n;
+            for(int j = 1; j < n-1; j++){
+                glassVals[i][j] = glassVals[i][j-1] + increment;
+            }
+            glassVals[i][n-1] = glassExtrema[1][i];
+        }
+        
+        double current = 0;
+        for(int i = 0; i < contIris.length; i++){
+            for(int j = 0; j < contIris[i].length; j++){
+                current = contIris[i][j];
+                if(irisVals[j][0] == -1){
+                    iris[i][j] = (int)current;
+                    continue;
+                }
+                int k = 0;
+                while(k < n && current <= irisVals[j][k]){
+                    k++;
+                }    
+                iris[i][j] = k;
+            }
+        }
+        for(int i = 0; i < contGlass.length; i++){
+            for(int j = 0; j < contGlass[i].length; j++){
+                current = contGlass[i][j];
+                if(glassVals[j][0] == -1){
+                    glass[i][j] = (int)current;
+                    continue;
+                }
+                int k = 0;
+                while(k < n && current <= glassVals[j][k]){
+                    k++;
+                }    
+                glass[i][j] = k;
             }
         }
     }
