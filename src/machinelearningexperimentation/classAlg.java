1a1f97c2983007d3e1793ab1a53349017d3de848
freivalds1 <freivalds1@gmail.com>
2016-11-10 17:54:58 -0700

    Template Method v1.0
    The  basic process all algorithms should follow, so everyone can focus on the actual algorithm's implementation.
    the String[][]s used by this template may be replaced depending on how we finalize handling of our data sets. 
    The very generic print statement at the conclusion of the run method will likely be refined.
    The run method requires a filled data set in a 2d array as input to operate.

--- src/machinelearningexperimentation/classAlg.java
+++ src/machinelearningexperimentation/classAlg.java
@@ -5,6 +5,8 @@
  */
 package machinelearningexperimentation;
 
+import static java.lang.Math.random;
+
 /**
  *
  * @Ryan Freivalds s53q433
@@ -20,28 +22,30 @@ public abstract class classAlg {
     String[][] sortSet; //represents our raw input
     String[][] readySet; //uses the filled/read in input to produce an array representing our training and testing set.
     String[][] trainingSet;
-    String[][] testSet; 
+    String[][] answerKey; //the test set we do not change, to compare our test set to
+    String[][] testSet;
     
 //might be a pain to read in all those values-- since we'll need to differentiate what we want the variables to be
 //or we take the time to hard code them in, unless someone is expereience in how to read in largs amounts of data.
     
-    public void run(){//***THIS OCCURES FOR EVERY ALGORITHM IMPLEMENTATION.***
+    public void run(String[][] input){//***THIS OCCURES FOR EVERY ALGORITHM IMPLEMENTATION.***
         /*This method represents the general outline of the process every algorithm will make use of.*/
         //fillSet(sortSet[]); //fills in any missing values, if nessissary, for the dataset.
         //Fillset will need to occure before run is called, to ensure the exact same filled in values are used for each algorithm.
        
-        sortSet = readSet(null); //accepts our data for local object use. 
+        sortSet = readSet(input); //accepts our data for local object use. 
         
         readySet = newSets(sortSet); 
         /*randomly seperates our data entries into a test and training sets. 
-        The training set will be the first half of the array and the testing set will be the later half.
+        The training set will be the first half of the array (rounding down) and the testing set will be the later half (rounding up).
         This will be  be called multiple times by the sorting alg.*/
         
-        classifyTest(readySet);
+        double results = classifyTest(readySet);
         /*
         Encompases the sorting algorithm's execution (another method in each class) coupled with multiple train/test runs
         returns our accuracy, or issues that occured while sorting.
         */
+        System.out.println("Accuracy for this algorithm was" + results + "%.");
     } 
     
     
@@ -49,36 +53,37 @@ public abstract class classAlg {
     public String[][] readSet(String[][] newInput){
         //needs to find or read in one data set for the algorithm to run on, and put it in an array.
         //returns that array for use.
-	    
-        return null;
+        //THIS MUST BE THE ARRAY WITH FILLED IN VALUES, IF NESSESSARY TO DO SO.
+        return newInput;
     }
-    
+    /*
     public void fillSet(String[][] dataEntries){
         //returns nothing, as it fills in any empty values for use in all future runs.
         //this may need to occure "outside" of this process, because EVERY algorithm needs to use the same filled in values.
         //enless we can ensure this method fills in the entries with the same values each time (unlikely).
     }
-
+*/
     private String[][] newSets(String[][] sortSet) {
         //This method represents our 5 x 2 "folding" for cross validation.
         //All this method does is take our filled in input array, and shuffles the indexes into a new array, using every index
         //the first half of this new array represents out training set, the last half our testing set.
         //This method will be called many times while an algorithm is being assessed.
 	String[][] shuffle = new String[sortSet.length][sortSet[1].length];
-	String[][] temp = new String[sortSet.length][sortSet[1].length]
+	String[][] temp = new String[sortSet.length][sortSet[1].length];
 	int rand = 0;
-	    for (int k = 0; k<3; k++)
+	    for (int k = 0; k<3; k++){
 		    for (int i = 0; i<sortSet.length; i++){
-			 rand = (int)math.random();   
+			 rand = (int)(random()*sortSet.length);   
 			 temp[i] = sortSet[i];
 			 sortSet[i] = sortSet[rand];
 			 sortSet[rand] = temp[i];
 		    }
 		}
+            Seperate();
         return shuffle;
     }
     
-    private void Seperate(){
+    private void Seperate(){//AUTOMATICALLY CALLED BY newSets()!
         //helper method that cuts our sortSet in half, actually setting trainingSet = to the first half and test set = to the last half
         //should be it's own method, because it will need to happen many times
         //returns nothing, as it sets the public variables equal to the halves they need to be
@@ -86,7 +91,11 @@ public abstract class classAlg {
 		trainingSet[i] = readySet[i];
 	   }
 	   for(int k =(int)(readySet.length)/2; k < readySet.length; k++){
+               //the classification for the testset is copied over, because we don't know nessissarily know where it is in the array. 
+               //we can clear it later or just write over it.
 		testSet[k] = readySet[k];
+                //the answer key is never changed, so we can compare the testSet to something to see if we were right or wrong.
+                answerKey[k] = readySet[k];
 	   }
 		   
     }
@@ -102,15 +111,17 @@ public abstract class classAlg {
         for (int i=0; i < 100; i++){
         Seperate();
         accuracy = algorithm(trainingSet, testSet);
+        System.out.println("Accuracy for trial #" + i + " was " + accuracy + "%."); //testline
         average = average + accuracy;
         trailCount++;
         }
         average = average/trailCount;
-        return 0;
+        return average;
     }
     
     public abstract double algorithm(String[][] train, String[][] test);
         //implemented by each individual class/algorithm. Not here. returns the accuracy for a single training/test set run
+        //When finished, used test[][] and compare it to answerKey[][] to find out how many were right/wrong, and return the value as the %right. 
     
     
     
