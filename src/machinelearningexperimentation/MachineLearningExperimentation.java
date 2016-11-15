9282c2c9a9df686df540d8faf86d4d840d750dbc
LizzieHerman <lizziemn96@gmail.com>
2016-11-14 17:40:09 -0700

    made testing possible

--- src/machinelearningexperimentation/MachineLearningExperimentation.java
+++ src/machinelearningexperimentation/MachineLearningExperimentation.java
@@ -9,29 +9,39 @@ package machinelearningexperimentation;
  */
 
 import java.io.*;
-import java.util.Random;
+import java.util.*;
 public class MachineLearningExperimentation {
+    
+    /**
+     * the last integer of each array is the classification of that entry
+     */
     static int[][] cancer = new int[699][11]; // missing values are recorded in array -1
     static double[][] contGlass = new double[214][11]; // all double values, except id num and class which are ints
     static double[][] contIris = new double[150][5]; // Iris-setosa = 1; Iris-versicolor = 2; Iris-virginica = 3
     static int[][] soybean = new int[47][36]; // class values D1-D4 to 1-4
-    static char[][] vote = new char[435][17]; // republican = r; democrat = d
+    static int[][] vote = new int[435][17]; // republican = 1; democrat = 2; y = 1; n = 0; ? = -1
     static int[][] glass = new int[214][11]; // for when we discretize the data
     static int[][] iris = new int[150][5]; // for when we discretize the data
     
 
     public static void main(String[] args) {
-       readFiles();
-       // TO-DO get rid of the unknown values
-       replaceUnknowns();
-       // TO-DO convert continuous values to discrete ones
-       discretize(10);
-       // split data into training and data sets
-       
-       // Run these for each algorithm
-       // TO-DO run training set
-       // TO-DO run test set
-       // TO-DO check how test set did
+        // read txt files into arrays
+        readFiles();
+        // get rid of the unknown values
+        replaceUnknowns();
+        //  convert continuous values to discrete ones
+        discretize(10);
+        // split data into training and data sets
+        classToLast();
+        // Run these for each algorithm
+        int[][] datasets[] = {cancer, glass, iris, soybean, vote};
+        
+        classAlg[] algorithms = {new NearestNeighbor(), new ID3(), new NaiveBayes(), new TAN()};
+        for(int[][] dataset : datasets){
+            for(classAlg algorithm : algorithms){
+                algorithm.run(dataset);
+            }
+        }
     }
     
     static void readFiles(){
@@ -114,7 +124,26 @@ public class MachineLearningExperimentation {
             while (line != null) {
                 split = line.split(cvsSplitBy);
                 for(int j = 0; j < vote[i].length; j++){
-                    vote[i][j] = split[j].charAt(0);
+                    char current = split[j].charAt(0);
+                    switch(current){
+                        case 'y':
+                            vote[i][j] = 1;
+                            break;
+                        case 'n':
+                            vote[i][j] = 0;
+                            break;
+                        case '?':
+                            vote[i][j] = -1;
+                            break;
+                        case 'r':
+                            vote[i][j] = 1;
+                            break;
+                        case 'd':
+                            vote[i][j] = 2;
+                            break;
+                        default:
+                            vote[i][j] = 10;
+                    }       
                 }
                 i++;
                 line = br.readLine();
@@ -196,6 +225,16 @@ public class MachineLearningExperimentation {
                 }    
                 glass[i][j] = k;
             }
+        }
+    }
+    
+    static void classToLast(){
+        for(int i = 0; i < vote.length; i++){
+            int classification = vote[i][0];
+            for(int j = 0; j < vote[i].length - 1; j++){
+                vote[i][j] = vote[i][j+1];
+            }
+            vote[i][16] = classification;
         }
     }
 }
