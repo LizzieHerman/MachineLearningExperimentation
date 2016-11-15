21ea0f0e7d4ebcf229184cc02af3b513b535f166
LizzieHerman <lizziemn96@gmail.com>
2016-11-14 17:44:06 -0700

    made testing possible

--- src/machinelearningexperimentation/ID3.java
+++ src/machinelearningexperimentation/ID3.java
@@ -6,9 +6,20 @@ package machinelearningexperimentation;
  * @Ryan Freivalds s53q433
  */
 public class ID3 extends classAlg {
+
+    @Override
+    public int[] algorithm(int[][] train, int[][] test) {
+        // TO-DO train your algorithm
+        int[] classes = new int[test.length];
+        // TO-DO test your algorithm
+        return classes;
+    }
     //needs a constructor to accept a dataset that's been filled if needed, and it's own classification implementation
 
-    public double algorithm(dataObject[] train, dataObject[] test){
-         return 0;
+    @Override
+    public String getName() {
+        return "ID3";
     }
+
+    
 }
