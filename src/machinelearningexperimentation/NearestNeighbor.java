ea3457e63ed922457d4de8b1137b03e9e1086e48
freivalds1 <freivalds1@gmail.com>
2016-11-15 18:07:23 -0700

    Update v1.2
    Compiles and "works" with the new main method and data read in.

--- src/machinelearningexperimentation/NearestNeighbor.java
+++ src/machinelearningexperimentation/NearestNeighbor.java
@@ -6,39 +6,39 @@ import static java.lang.Math.abs;
 import static java.lang.Math.sqrt;
 
 /**
- * @author Lizzie Herman 
- * @author Ryan Freivalds
+ *
+ * @author Lizzie Herman Ryan Freivalds
  */
 public class NearestNeighbor extends classAlg {
 
     //needs a constructor to accept a dataset that's been filled if needed, and it's own classification implementation
     //to allow vdm to use these variables:
     private ArrayList output[]; //how many values an attribute can have
-    private String[][] train;
+    private int[][] train;
     private ArrayList classes; //how many classifications
     private int[][] outputCounter;//contains the how many of a specific outputs for each attribute of our dataset, for whatever attribute is held at index *          
 
     @Override
-    public double algorithm(String[][] train, String[][] test) { //current implementation assumes String[*][0] holds our classification for an entry.
+    public int[] algorithm(int[][] train, int[][] test) { //current implementation assumes int[*][0] holds our classification for an entry.
         int k = 8; //how many nearest neighbours we compare against
-
+        int[] results = new int[test.length];
         //because the training for nearest neighbour is simply storing out training sets for comparison, we have to extract the details of outcomes.
         this.classes = new ArrayList(); //how many classifications
         ArrayList[] output = new ArrayList[train[0].length]; //how many values an attribute can have
         // System.out.println("arraylist array size of "+ train[0].length);
-        for (int i = 1; i < train[0].length; i++) {
+        for (int i = 0; i < train[0].length-1; i++) {
             output[i] = new ArrayList();
         }
         this.output = output;
         this.train = train;
-        System.out.println("Detected " + train[0].length + " attribute fields."); //testline
-        for (int n = 0; n < train.length; n++) {
-            if (!classes.contains(train[n][0])) { //if this classification hasn't been seen
-                classes.add(train[n][0]); //then add it to our list of classifications
+        //System.out.println("Detected " + train[0].length + " attribute fields."); //testline
+        for (int n = 0; n < train.length; n++) {//for all training entries
+            if (!classes.contains(train[n][train[0].length-1])) { //if this classification hasn't been seen
+                classes.add(train[n][train[0].length-1]); //then add it to our list of classifications
             }
         }
-        System.out.println("Detected " + classes.size() + " classifications."); //testlines
-        for (int i = 1; i < train[0].length; i++) {//Counting through the attribute TYPES. n=1 because we are not currently conserned with the classification of either set. 
+       // System.out.println("Detected " + classes.size() + " classifications."); //testlines
+        for (int i = 0; i < train[0].length-1; i++) {//Counting through the attribute TYPES. n=1 because we are not currently conserned with the classification of either set. 
             //System.out.println("testing initialization of output["+i+"]: "+ output[i]);
             //output[i] = new ArrayList();
             for (int m = 0; m < train.length; m++) { //checking how many outcomes for a SINGLE TYPE of attribute across MULTIPLE DATA ENTRIES
@@ -46,18 +46,18 @@ public class NearestNeighbor extends classAlg {
                     output[i].add(train[m][i]); //then add it to our list of outcomes for that SPECIFIC attribute [i]
                 }//DIFFICULTY: The number of values for an attribute is not likely to be uniform, making it difficult to instanciate properly sized 2d array to count instances of outcomes.
             }
-            System.out.println("Detected " + output[i].size() + " different outcomes for attribute " + i + ".");//testlines
+           // System.out.println("Detected " + output[i].size() + " different outcomes for attribute " + i + ".");//testlines
         }
         //now we know all possible outcomes, but we still need to tally know how many.
         //this is the most brutal nesting of loops I've ever done. IF THERE IS A PROBLEM IT'S PROBABLY HERE.        
         outputCounter = new int[train[0].length][];
         //outputCounter[a][b] where [a] is the attribute, [b] is the type of outcome witnessed, holding the number of times it was seen.
         //we know how many attributes there are right now, but not nessessarily how many outcomes there were per attribute.
-        for (int a = 1; a < train[0].length; a++) {//Counting through the attribute TYPES (excludes the classification).
+        for (int a = 0; a < train[0].length-1; a++) {//Counting through the attribute TYPES (excludes the classification).
             outputCounter[a] = new int[output[a].size()]; //so for attribute [a], outputCounter[a][] becomes outputCounter[a][how many kinds of output were seen]
             for (int m = 0; m < train.length; m++) { //checking how many outcomes for a SINGLE TYPE of attribute across MULTIPLE DATA ENTRIES
                 for (int b = 0; b < output[a].size(); b++) { //for each outcome for that attribute
-                    if (train[m][a] == output[a].get(b)) { //if that outcome was seen for that attribute
+                    if (train[m][a] == (int)output[a].get(b)) { //if that outcome was seen for that attribute
                         outputCounter[a][b] += 1; //iterate that outcome. This will give us the total number of times that outcome per attribute was seen.
                     }
                 }
@@ -70,7 +70,7 @@ public class NearestNeighbor extends classAlg {
         double hit = 0;
         double inf = Double.POSITIVE_INFINITY;
         for (int i = 0; i < test.length; i++) {// for every entry in our TESTing set
-            String[][] candidate = new String[k][];//holds our ENTRIES for our nearest neighbours
+            int[][] candidate = new int[k][];//holds our ENTRIES for our nearest neighbours
             double[] canddist = new double[k]; //candidate distences to our test point.
             for(int x = 0; x<k; x++){
                 canddist[x] = inf; //filling distences with infinity
@@ -78,8 +78,9 @@ public class NearestNeighbor extends classAlg {
             for (int j = 0; j < train.length; j++) {// for every entry in our TRAINING set
                 //System.out.println("comparing new attributes from entry number "+ j +"."); //good printline
                 double[] singdist = new double[test[i].length];//short for "single distences, represents the distences between individual attribues, to be used to calculate the distences of entries from one another.
-                for (int a = 1; a < test[i].length; a++) { //for every attribute of both entries that must be compared (excludes the classification).
-                    singdist[a] = vdm(a, test[i][a], train[j][a]); //add a new value to the double array for individual attribute distences here.
+                for (int a = 0; a < test[i].length-1; a++) { //for every attribute of both entries that must be compared (excludes the classification).
+                    int entry = test[i][a];
+                    singdist[a] = vdm(a, entry, train[j][a]); //add a new value to the double array for individual attribute distences here.
                 } //end of "for every attribute" 
 
                 
@@ -93,7 +94,7 @@ public class NearestNeighbor extends classAlg {
                 //insert sort into our array of nearest neighbours?  
               // System.out.println("comparing new distence " + entrydist + " to old candidate distence of " + canddist[0]);//IMPORTANT testline
                 for (int l = 0; l < k; l++) { //the minus 1 might be unnessissary
-                    //System.out.println("comparing new distence " + entrydist + " to old candidate distence of " + canddist[l]);//semi-IMPORTANT testline
+                    //System.out.println("comparing new distence " + entrydist + " to old candidate distence of " + canddist[l]);//good printline
                     /*
                     if (canddist[l] == 0.0) {
                         canddist[l] = entrydist;
@@ -104,8 +105,8 @@ public class NearestNeighbor extends classAlg {
                         //System.out.println("comparing new distence "+entrydist+ " to old candidate distence of "+canddist[0]);//testline
                         double tempdist = canddist[l];
                         double tempdist2 = inf;
-                        String[] temp = candidate[l];
-                        String[] temp2 = null;
+                        int[] temp = candidate[l];
+                        int[] temp2 = null;
                         if(l != k-1){
                             tempdist2 = canddist[l + 1];
                             temp2 = candidate[l+1];
@@ -154,7 +155,7 @@ public class NearestNeighbor extends classAlg {
                 for (int a = 0; a < classes.size(); a++) {//for all our classifications
                     //System.out.println("classification number "+ a+":"); //testline
                     // System.out.println("comparing classification of "+candidate[nn][0]+" to "+ classes.get(a)); //testline
-                    if (candidate[nn][0].equals(classes.get(a))) { //if our current nearest neighbour had that classification
+                    if (candidate[nn][test[0].length-1] == (int)classes.get(a)) { //if our current nearest neighbour had that classification
                         vote[a]++; //increment that vote, then stop.
                         break;
                     }
@@ -168,26 +169,30 @@ public class NearestNeighbor extends classAlg {
                     }
                 }
             }
-            //System.out.println("Assigning entry "+ i + ", " + test[i][0]+ ", the classification of " +classes.get(winner) +".");
-            if (test[i][0].equals(classes.get(winner))) {//if our newly assigned classification is what the entry actually was
+            //System.out.println("Assigning entry "+ i + ", " + test[i][test[0].length-1]+ ", the classification of " +classes.get(winner) +"."); //good println
+            if (test[i][test[0].length-1] == (int)(classes.get(winner))) {//if our newly assigned classification is what the entry actually was
                 hit++; //increment our number of correct guesses.
             }
-            test[i][0] = (String) classes.get(winner); //we have now made our choice for a single test entry
+            
+            results[i] = (int)classes.get(winner);
+           // test[i][test[0].length-1] = (int) classes.get(winner); //we have now made our choice for a single test entry
         } //end of "for every entry" TESTING
         //now tally our accuracy
         double tally = 0;
         for (int i = 0; i < test.length; i++) {//for every test entry
             tally++;
         }
+        /*
         System.out.println("");
         System.out.println("----------------------------------------------------------");
         System.out.println("For this test set we guessed correctly " + hit + " out of " + tally + " entries. Our accuracy was " + hit / tally + "%.");
         System.out.println("----------------------------------------------------------");
         System.out.println("");
-        return hit / tally;
+        */
+        return results;
     }
 
-    public double vdm(int attr, String v1, String v2) { //v for "value," attr is the index of the "attribute" we're finding distences for the two values being compaired
+    public double vdm(int attr, int v1, int v2) { //v for "value," attr is the index of the "attribute" we're finding distences for the two values being compaired
         //value difference metric method, called on by the algorithm
         double distence = 0;
         int v1outCountIndex = -5000; //gives us the proper index of outputCounter[attr][] to reference for the vdm formula. This value is found in the loop below.
@@ -219,10 +224,10 @@ public class NearestNeighbor extends classAlg {
             int val2 = 0;
             for (int e = 1; e < train.length; e++) {//Counting through the data entries
                 //train[e][attr]
-                if (train[e][attr].equals(v1)) { //if that entry's attribute equals one of the values we're compairing. 
+                if (train[e][attr] ==(v1)) { //if that entry's attribute equals one of the values we're compairing. 
                     val1++;
                 }
-                if (train[e][attr].equals(v2)) { //if that entry's attribute the other value we're compairing. 
+                if (train[e][attr] ==(v2)) { //if that entry's attribute the other value we're compairing. 
                     val2++;
                 }
             }
@@ -252,15 +257,10 @@ public class NearestNeighbor extends classAlg {
     }
 
     @Override
-    public int[] algorithm(int[][] train, int[][] test) {
-        // TO-DO train your algorithm
-        int[] classes = new int[test.length];
-        // TO-DO test your algorithm
-        return classes;
+    public String getName() {
+        return "nearest Neighbor";
+       // throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        
     }
 
-    @Override
-    public String getName() {
-        return "Nearest Neighbor";
-    }
 }
