aa909371989ed7f0bb303b9fc6c7a644d0557aeb
LizzieHerman <lizziemn96@gmail.com>
2016-11-16 12:43:30 -0700

    most recent TAN, not functional yet

--- src/machinelearningexperimentation/TAN.java
+++ src/machinelearningexperimentation/TAN.java
@@ -6,6 +6,7 @@ package machinelearningexperimentation;
  */
 
 import java.util.ArrayList;
+import java.util.Arrays;
 
 public class TAN extends classAlg{
 
@@ -18,16 +19,17 @@ public class TAN extends classAlg{
     }
     
     private void train(int[][] dataset){
-        int[] countVal = new int[dataset[0].length];
-        ArrayList[] vals = new ArrayList[dataset[0].length];
-        String[] nameVal = new String[dataset[0].length - 1];
-        for(int i = 0; i < dataset[0].length; i++){
+        int numAttr = dataset[0].length - 1;
+        int[] countVal = new int[numAttr + 1];
+        ArrayList[] vals = new ArrayList[numAttr + 1];
+        String[] nameVal = new String[numAttr];
+        for(int i = 0; i <= numAttr; i++){
             vals[i] = new ArrayList();
-            if(i >= nameVal.length) continue;
+            if(i >= numAttr) continue;
             nameVal[i] = Integer.toString(i);
         }
         for(int i = 0; i < dataset.length; i++){
-            for(int j = 0; j < dataset[i].length; j++){
+            for(int j = 0; j <= numAttr; j++){
                 int cur = dataset[i][j];
                 if(! vals[j].contains(cur)){
                     vals[j].add(cur);
@@ -35,6 +37,15 @@ public class TAN extends classAlg{
                 }
             }
         }
+        System.out.println(Arrays.toString(countVal));
+        for(int i = 0; i < numAttr; i++){
+            for(Object v : vals[i]){
+                for(Object c : vals[numAttr]){
+                    //blah
+                }
+            }
+        }
+        
         Graph graph = new Graph(countVal.length - 1);
         graph.createFullGraph(nameVal);
     }
